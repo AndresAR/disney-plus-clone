98d50393abedf66baea313665c103d74846174fe
Andrés Arias Rauld <andresariasrauld@gmail.com>
2021-05-25 14:36:33 -0400

    Adding movies detail from db

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,37 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import { useParams } from 'react-router-dom';
+import db from '../firebase'
+import { useHistory } from 'react-router-dom';
 
 function Detail() {
+    const { id } = useParams();
+    const history = useHistory();
+    const [ movie, setMovie ] = useState();
+
+    useEffect(()=>{
+        db.collection("movies")
+        .doc(id)
+        .get()
+        .then((doc)=>{
+            if(doc.exists) {
+                setMovie(doc.data());
+                console.log(doc.data());
+            } else {
+                history.push('/')
+            }
+        })
+    }, [])
+
+    console.log(movie)
     return (
         <Container>
-            <Background>
-                <img src='https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/4F39B7E16726ECF419DD7C49E011DD95099AA20A962B0B10AA1881A70661CE45/scale?width=1440&aspectRatio=1.78&format=jpeg' alt='' />
-            </Background>
-            <ImgTitle>
-                <img src='https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/D7AEE1F05D10FC37C873176AAA26F777FC1B71E7A6563F36C6B1B497CAB1CEC2/scale?width=1440&aspectRatio=1.78' alt='' />
-            </ImgTitle>
-            <Controls>
-                <PlayButton>
-                    <img src='/images/play-icon-black.png' alt='' />
-                    <span>PLAY</span>
-                </PlayButton>
-                <TrailerButton>
-                    <img src='/images/play-icon-white.png' alt='' />
-                    <span>Trailer</span>
-                </TrailerButton>
-                <AddButton>
-                    <span>+</span>
-                </AddButton>
-                <GroupWatchButton>
-                    <img src='/images/group-icon.png' alt='' />
-                </GroupWatchButton>
-            </Controls>
-            <SubTitle>
-                2018 • 7m • Fantasy, Kids, Animation
-            </SubTitle>
-            <Description>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
-            </Description>
+            {movie && (
+                <>
+                    <Background>
+                        <img src={movie.backgroundImg} alt='' />
+                    </Background>
+                    <ImgTitle>
+                        <img src={movie.titleImg} alt='' />
+                    </ImgTitle>
+                    <Controls>
+                        <PlayButton>
+                            <img src='/images/play-icon-black.png' alt='' />
+                            <span>PLAY</span>
+                        </PlayButton>
+                        <TrailerButton>
+                            <img src='/images/play-icon-white.png' alt='' />
+                            <span>Trailer</span>
+                        </TrailerButton>
+                        <AddButton>
+                            <span>+</span>
+                        </AddButton>
+                        <GroupWatchButton>
+                            <img src='/images/group-icon.png' alt='' />
+                        </GroupWatchButton>
+                    </Controls>
+                    <SubTitle>
+                        { movie.subTitle }
+                    </SubTitle>
+                    <Description>
+                        { movie.description }
+                    </Description>
+                </>
+            )}                    
         </Container>
     )
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 import { selectMovies } from '../features/movie/movieSlice';
 import { useSelector } from 'react-redux';
 
 function Movies() {
     const movies = useSelector(selectMovies);
-    console.log('this is movies', movies)
-
     return (
         <Container>
             <h4>Recommended for You</h4>
             <Content>
                 { movies && 
                     movies.map((movie) => (
+
                         <Wrap key={movie.id}>
-                            <img src={movie.cardImg} alt={movie.title} />
+                            <Link to={ `/detail/${ movie.id }` }>
+                                <img src={movie.cardImg} alt={movie.title} />
+                            </Link>                            
                         </Wrap>
                     ))
                 }
